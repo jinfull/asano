6c32fffdf5c0cff78bc68bc6e5e5b29590c23678
jinfull <jinfull@gmail.com>
2019-04-30 14:20:11 -0700

    Fixing webpack file for entry file and output files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+    entry: './frontend/entry.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascript'),
+        filename: './bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
