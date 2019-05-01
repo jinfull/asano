47483a37ec6fb35cc048d2dda8212f6bec102234
jinfull <jinfull@gmail.com>
2019-05-01 11:02:45 -0700

    Introduced entities reducer for organization

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            login(@user)
+            login!(@user)
             render "api/users/show"
         else
             render json: @user.errors.full_messages, status: 422
@@ -13,6 +13,6 @@ class Api::UsersController < ApplicationController
     private
     
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :email, :password, :team_id, :first_name, :last_name)
     end
 end
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import entitiesReducer from './entities_reducer';
+import sessionReducer from './session_reducer';
 
 export default combineReducers({
-    entitiesReducer
+    sessionReducer
 });
