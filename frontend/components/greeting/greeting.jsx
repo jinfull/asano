50db4d74ae7c21a2a5d7e5d3e0b53ff2e16a2ea2
jinfull <jinfull@gmail.com>
2019-05-02 23:15:13 -0700

    Working on header adding header logo

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+
 
 
 const Greeting = ({ currentUser, logout }) => {
@@ -11,7 +11,7 @@ const Greeting = ({ currentUser, logout }) => {
             <Link to='signup'>Sign Up!</Link>            
         </nav>
     );
-
+        
     const personalGreeting = () => (
         <hgroup className='header-group'>
             <h2 className='header-name'>Hi, {currentUser.first_name}!</h2>
@@ -19,7 +19,15 @@ const Greeting = ({ currentUser, logout }) => {
         </hgroup>
     );
 
-    return currentUser ? personalGreeting() : sessionLinks();
+    
+    return (
+        <div>
+            {/* <div>{logo}</div> */}
+            <div className='header-logo'>mylogo</div>
+            <div>{currentUser ? personalGreeting() : sessionLinks()}</div>
+        </div>
+    )
+    
 }
 
 export default Greeting;
