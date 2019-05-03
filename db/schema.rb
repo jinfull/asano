28591eb82df23f081752b885ad689edc40374ca0
jinfull <jinfull@gmail.com>
2019-05-03 10:26:45 -0700

    Slight change to width of login-form-container

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_002845) do
+ActiveRecord::Schema.define(version: 2019_05_02_223334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.integer "owner_id", null: false
+    t.integer "team_id", null: false
+    t.string "name", null: false
+    t.boolean "completed", null: false
+    t.text "description"
+    t.date "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_projects_on_owner_id"
+    t.index ["team_id"], name: "index_projects_on_team_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "team_id"
