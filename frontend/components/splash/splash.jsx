6be0cf0bc301785f6adce2db460923ee11715b1d
jinfull <jinfull@gmail.com>
2019-05-03 17:28:56 -0700

    Polish frontend (non-logged-in) splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -34,7 +34,7 @@ class Splash extends React.Component {
                         <h1 id="splash-h1">Make more time for the work that matters most</h1>
                         <h2 id="splash-h2">Asano is the work management platform teams use to stay focused on the goals, projects, and daily tasks that grow business.</h2>
                         <div className='splash-form'>
-                            <input id='splash-started-input' type="text" onChange={this.update('email')}/>
+                            <input id='splash-started-input' type="text" onChange={this.update('email')} placeholder='name@company.com' />
                             <Link to={`/signup/${this.state.email}`}>
                                 <button id='splash-started-button'>Try for free</button>
                             </Link>
