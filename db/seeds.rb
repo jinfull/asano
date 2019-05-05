b1c03cfcb053cf1b93d56b5860b43ca4cb38448e
jinfull <jinfull@gmail.com>
2019-05-05 13:13:05 -0700

    Added initial seed user (demo user login) and understand how to use rails db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(email: 'jinfull2@gmail.com', first_name: 'Jinfull', last_name: 'Jeng', password: 'jinfull')
