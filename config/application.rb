501dd7991fb9880e76b83971d96495e27e7bfbb8
jinfull <jinfull@gmail.com>
2019-05-06 01:11:16 -0700

    Add 'config.assets.initialize_on_precompile = false' in application.rb for heroku push

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Asano
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
