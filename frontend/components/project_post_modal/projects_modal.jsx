8db07b6e6eda5a29877dc72e958c4e319a8f77b8
jinfull <jinfull@gmail.com>
2019-05-07 09:48:49 -0700

    Finish project creation modal components and styling

--- frontend/components/project_post_modal/projects_modal.jsx
+++ frontend/components/project_post_modal/projects_modal.jsx
@@ -8,28 +8,29 @@ function Modal({ modal, closeModal }) {
     if (!modal) {
         return null;
     }
-    // let component;
-    // switch (modal) {
-    //     case 'login':
-    //         component = <LoginFormContainer />;
-    //         break;
-    //     case 'signup':
-    //         component = <SignupFormContainer />;
-    //         break;
-    //     default:
-    //         return null;
-    // }
+
     return (
         <div className="modal-background" onClick={closeModal}>
             <div className="modal-child" onClick={e => e.stopPropagation()}>
                 <div className='modal-top-level'>
                     <h1 className='create-project'>Create Project</h1>
-                    <form>
-                        <label className='project-form-label'>Name</label>
-                        <input className='project-input' id='project-input-name' type='text' />
-
-                        <label className='project-form-label'>Description</label>
-                        <textarea className='project-input' />
+                    <form className='project-form-container'>
+                        <div className='project-name-div'>
+                            <label className='project-form-label' id='project-submit-name'>Name</label>
+                            <br />
+                            <input className='project-input' id='project-input-name' type='text' />
+                            <br/>
+                        </div>
+                        <div className='project-description-div'>
+                            <label className='project-form-label' id='project-submit-description'>Description</label>
+                            <br />
+                            <textarea className='project-input' />
+                            <br />
+                        </div>
+                        <div className='button-row'>
+                            <button className='create-project-submit'>Create Project</button>
+                            <button className='modal-cancel' onClick={closeModal}>Cancel</button>
+                        </div>
                     </form>
                 </div>
             </div>
