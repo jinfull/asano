cb2884ff073972fe0bd384e8ea9ef8163d6546c7
jinfull <jinfull@gmail.com>
2019-05-09 11:02:43 -0700

    added task show skeleton

---
+++ frontend/components/task_show/task_show.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class TaskShow = (props) => {
+    return (
+        <div>hello</div>
+    )
+}
---
+++ frontend/components/task_show/task_show_container.jsx
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import TaskShow from './task_show';
+
+const mSTP = (state, ownProps) => {
+
+};
+
+const mDTP = dispatch => {
+
+};
+
+export default connect(mSTP, mDTP)(TaskShow);
