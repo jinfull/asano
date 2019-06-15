03e09431e2afa6eecf4f7efd6c78dbbf6d7b59fb
jinfull <jinfull@gmail.com>
2019-06-14 21:54:39 -0700

    Fix double click of forcing mark-complete button to change

--- frontend/components/task_show/task_show.jsx
+++ frontend/components/task_show/task_show.jsx
@@ -5,7 +5,7 @@ class TaskShow extends React.Component {
     constructor(props) {
         super(props);
         // this.state = this.props.task;
-        this.state = { completed: false };
+        // this.state = { completed: false };
         this.updateCompleted = this.updateCompleted.bind(this);
     }
 
@@ -17,12 +17,18 @@ class TaskShow extends React.Component {
         // console.log(this.props.task);
         // console.log(this.state);
 
-        this.setState({ completed: value });
+        // console.log('hello', value);
 
-        let taskCopy = Object.assign({}, this.props.task);
-        taskCopy.completed = this.state.completed;
+        // this.setState({ completed: value });
 
-        this.props.updateTask(taskCopy);
+        // let taskCopy = Object.assign({}, this.props.task);
+        // this.props.task.completed = false;
+        // console.log(this.props.task)
+        // taskCopy.completed = value; // this.state.completed;
+
+        // console.log(this.state.completed);
+
+        this.props.updateTask({...this.props.task, completed: value});
     }
     
     render() {
@@ -61,10 +67,6 @@ class TaskShow extends React.Component {
             <div className='task-show-top'>
                 <div id="task-show-mark-complete">
                     {(task.completed) ? completed() : markComplete()}
-                    {/* <button id="task-mark-complete-button">
-                        <svg className="mark-complete-check-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504.5 75.5c-10-10-26.2-10-36.2 0L161.6 382.2 43.7 264.3c-10-10-26.2-10-36.2 0 -10 10-10 26.2 0 36.2l136 136c10 10 26.2 10 36.2 0L504.5 111.7C514.5 101.7 514.5 85.5 504.5 75.5z" /></svg>
-                        Mark Complete
-                    </button> */}
                 </div>
                 <div id="task-show-header">
                     <div id='task-show-name'>{task.name}</div>
