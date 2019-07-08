58b4a70ea990d9cc061489f6438a9175c30baa5d
jinfull <jinfull@gmail.com>
2019-07-08 13:35:14 -0700

    Remove console accessible functions from application (window.getState, etc)

--- frontend/asano.jsx
+++ frontend/asano.jsx
@@ -22,12 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
         store = configureStore();
     }
 
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
 
-    window.signup = signup;
-    window.login = login;
-    window.logout = logout;
+    // window.signup = signup;
+    // window.login = login;
+    // window.logout = logout;
 
 
     // window.signup = SessionApiUtil.signup;
